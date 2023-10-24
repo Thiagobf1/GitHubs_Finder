69b6da7bff785a76bfcf11996c9316b13845c0be
Thiagobf1 <thiago.busifeijo@gmail.com>
2023-10-24 17:08:19 -0300

    Add KeyDown

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,32 +1,37 @@
-import { useState } from "react";
+import React, { useState, KeyboardEvent } from "react";
 import { BsSearch } from "react-icons/bs";
+import classes from "./Search.module.css";
 
-// Correção 1: Use as props corretamente.
 type SearchProps = {
   loadUser: (userName: string) => Promise<void>;
 };
 
-// Correção 2: Adicione as props como argumentos da função.
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
 
-  // Correção 2: Chame a função loadUser no evento onClick.
   const handleSearch = () => {
     loadUser(userName);
   };
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+    }
+  };
+
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
+      <div className={classes.search_container}>
         <input
           type="text"
           placeholder="Digite o nome do usuário"
-          value={userName} // Correção: Use 'value' para controlar o input.
+          value={userName}
           onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <button onClick={handleSearch}> {/* Correção: Chame a função handleSearch. */}
+        <button onClick={handleSearch}>
           <BsSearch />
         </button>
       </div>
