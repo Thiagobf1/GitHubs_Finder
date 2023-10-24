b44434c1052c11e92fc45cfb2e8eb4348d1f16fc
Thiagobf1 <thiago.busifeijo@gmail.com>
2023-10-24 16:31:09 -0300

    Correção Home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,32 @@
-import Search from "../components/Search"
-import { useState } from "react"
+import { UserProps } from "../Types/user";
+import Search from "../components/Search";
+import { useState } from "react";
 
 const Home = () => {
-  const {user, setUser} = useState(null);
+  // Correção: Use useState para criar um estado e uma variável para armazená-lo.
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`); // Correção: Use crases (`) para interpolação da string.
+
+    const data = await res.json();
+
+    // Correção: Atualize o estado com os dados do usuário.
+    setUser(data);
+  };
 
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
+      {/* Correção: Exibir informações do usuário, se disponível. */}
+      {user && (
+        <div>
+          <h2>User Information</h2>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
